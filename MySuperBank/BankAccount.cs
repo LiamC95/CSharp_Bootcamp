8eaf68ebe76379e21f569e3c9fb5933982c2e420
Liam Clarke <liamc950@gmail.com>
2022-09-03 17:52:32 +0100

    Started into another console app bankaccounts

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    internal class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions) {
+                    balance += item.Amount;
+                }
+                return balance;
+            } set
+            {
+
+            }
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+        private static int accountNumberSeed = 123467890;
+
+        public BankAccount(string name, decimal initialBalance) {
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Owner = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit (decimal ammount, DateTime date, string note)
+        {
+            if(ammount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(ammount), "Ammount of deposit must be positive");
+            }
+            var deposit = new Transaction(ammount, date, note);
+            allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawal(decimal ammount, DateTime date, string note)
+        {
+            if(ammount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(ammount), "Ammount of withdrawal must be positive");
+            }
+            if (this.Balance - ammount >= 0)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Liam Clarke", 12000);
+            var account1 = new BankAccount("Liam Clarke", 12000);
+            Console.WriteLine($"Account {account.Number} Created for {account.Owner} with balance {account.Balance}");
+            Console.WriteLine($"Account {account1.Number} Created for {account1.Owner} with balance {account1.Balance}");
+        }
+    }
+}
